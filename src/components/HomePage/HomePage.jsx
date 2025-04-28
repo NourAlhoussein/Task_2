f0003c2bde6eb2ffe7272ce6390867a05afbaf63
nour alhoussein <nour.alhoussein@axtro-soft.com>
2025-04-29 01:38:10 +0300

    modify the src of photos

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styles from "./HomePage.module.css";
 import { IoIosAdd } from "react-icons/io";
-import imageContainer from "/public/assets/images/Image Container.png";
+import imageContainer from "../../../public/assets/images/Image Container.png";
 function HomePage({ items }) {
   return (
     <div className={styles.HomePage}>
