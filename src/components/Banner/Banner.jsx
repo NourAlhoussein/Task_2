ec7ef2a9cf386ae58c270e0e754097a008d3a8da
nour alhoussein <nour.alhoussein@axtro-soft.com>
2025-04-29 10:58:36 +0300

    modify import photos

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styles from "./Banner.module.css";
 import { AiOutlineArrowRight } from "react-icons/ai";
-import abstract from "/assets/images/Abstract Design.png"
 
 function GrabSeat({ abstract2, group, group2 }) {
   return (
@@ -11,7 +10,7 @@ function GrabSeat({ abstract2, group, group2 }) {
           Admission is Open, Grab your seat now
         </div>
         <AiOutlineArrowRight className={styles.BannerArrow} />
-        <img className={styles.imageOne} src={abstract} alt="" />
+        <img className={styles.imageOne} src= "/Task_2/assets/images/Abstract Design.png" alt="" />
         <img className={styles.imageTwo} src={abstract2} alt="" />
         <img className={styles.imageThree} src={group} alt="" />
         <img className={styles.imageFour} src={group2} alt="" />
